bd41938f9f227a57f62ec69d88500b05ac649c43
Hank-mbp <cgh20xx@gmail.com>
2021-11-23 16:06:55 +0800

    接收params和新增 error layout

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>對不起出錯啦</h1>
+    <p>{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
+
+<style>
+</style>
---
+++ pages/item/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>接收URL参数 id</h1>
+    <hr />
+    <div v-if="this.$route.params.id">id:{{ this.$route.params.id }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$route.params)
+  },
+  validate({ params }) {
+    // params.id 只能為數值
+    return /^\d+$/.test(params.id)
+  },
+}
+</script>
+
+<style>
+</style>
